4e053033b28070e9b84ec31e1805088850b476f7
Nanxi <leenanxi@gmail.com>
2018-03-09 18:38:59 +0800

    enable l2tp

--- one-key-ikev2.sh
+++ one-key-ikev2.sh
@@ -239,14 +239,20 @@ function setup_strongswan(){
 --enable-eap-mschapv2 --enable-eap-tls --enable-eap-ttls --enable-eap-peap  \
 --enable-eap-tnc --enable-eap-dynamic --enable-eap-radius --enable-xauth-eap  \
 --enable-xauth-pam  --enable-dhcp  --enable-openssl  --enable-addrblock --enable-unity  \
---enable-certexpire --enable-radattr --enable-swanctl --enable-openssl --disable-gmp
+--enable-certexpire --enable-radattr --enable-swanctl --enable-openssl --disable-gmp \
+--enable-nat-transport  --disable-mysql  --disable-ldap  --disable-static  --enable-shared  \
+--enable-md4  --enable-eap-aka  --enable-eap-aka-3gpp2  --enable-eap-gtc  --enable-eap-sim  \
+--enable-eap-sim-file  --enable-eap-simaka-pseudonym  --enable-eap-simaka-reauth  --enable-eap-simaka-sql
 
     else
         ./configure  --enable-eap-identity --enable-eap-md5 \
 --enable-eap-mschapv2 --enable-eap-tls --enable-eap-ttls --enable-eap-peap  \
 --enable-eap-tnc --enable-eap-dynamic --enable-eap-radius --enable-xauth-eap  \
 --enable-xauth-pam  --enable-dhcp  --enable-openssl  --enable-addrblock --enable-unity  \
---enable-certexpire --enable-radattr --enable-swanctl --enable-openssl --disable-gmp --enable-kernel-libipsec
+--enable-certexpire --enable-radattr --enable-swanctl --enable-openssl --disable-gmp --enable-kernel-libipsec \
+--enable-nat-transport  --disable-mysql  --disable-ldap  --disable-static  --enable-shared  \
+--enable-md4  --enable-eap-aka  --enable-eap-aka-3gpp2  --enable-eap-gtc  --enable-eap-sim  \
+--enable-eap-sim-file  --enable-eap-simaka-pseudonym  --enable-eap-simaka-reauth  --enable-eap-simaka-sql  
 
     fi
     make; make install
